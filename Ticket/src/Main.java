3391f58ae6210476b337de449dbbd426b61e939e
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-02-01 19:26:08 +0300

    Update Main.java

--- Ticket/src/Main.java
+++ Ticket/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Ticket ticket = new Ticket("","Malgin","man","12.08.1995",null,null);
+        Ticket ticket = new Ticket("Evgeny","Malgin","man","12.08.1995",null,null);
         ticket.registration();
     }
 }
