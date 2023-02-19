97b3a83073f96b676bdc41f867c50a2c44afcf9d
Polyakov <d.polyakov@infomaximum.com>
2023-02-19 11:00:18 +0300

    add pattern proxy

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -2,23 +2,40 @@ public class Calculator implements Methods {
     private double a;
     private double b;
 
+    public Calculator() {}
+
     public Calculator(double a, double b) {
         this.a = a;
         this.b = b;
         check();
     }
 
+
+    @Override
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    @Override
+    public double sub(double a, double b) {
+        return a - b;
+    }
+
+    @Override
+    public double mul(double a, double b) {
+        return a * b;
+    }
+
     @Override
     public double div(double a, double b) {
-        double result = a/b;
+        double result = a / b;
         if (b == 0) {
             throw new ArithmeticException("Divisor cannot div by zero");
         }
-        System.out.println(result);
         return result;
     }
 
-    public void check(){
-        System.out.println(a +" " +  b);
+    public void check() {
+        System.out.println(a + " " + b);
     }
 }
--- Calculator/src/ProxyCalculator.java
+++ Calculator/src/ProxyCalculator.java
@@ -1,19 +1,32 @@
 public class ProxyCalculator implements Methods {
 
-    private double a;
-    private double b;
     private Calculator calculator;
 
-    public ProxyCalculator(double a, double b) {
-        this.a = a;
-        this.b = b;
+    public ProxyCalculator(Calculator calculator) {
+        this.calculator = calculator;
+    }
+
+    @Override
+    public double add(double a, double b) {
+        return calculator.add(a,b);
+    }
+
+    @Override
+    public double sub(double a, double b) {
+        return calculator.sub(a,b);
+    }
+
+    @Override
+    public double mul(double a, double b) {
+        double result = 0;
+        for(int i = 0; i < a; i++) {
+            result += b;
+        }
+        return result;
     }
 
     @Override
     public double div(double a, double b) {
-        if(calculator == null){
-            calculator = new Calculator(a, b);
-        } calculator.div(a, b);
         return a/b;
     }
 }
