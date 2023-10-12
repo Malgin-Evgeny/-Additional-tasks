d2047a1003f33d1d6965a693db4874eefbdabbf1
Мальгин Евгений Владимирович <115340327+Malgin-Evgeny@users.noreply.github.com>
2023-10-12 19:48:28 +0300

    Delete Ticket/src directory

--- Ticket/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Ticket ticket = new Ticket("Evgeny","Malgin","man","12.08.1995",null,null);
-        ticket.registration();
-    }
-}
